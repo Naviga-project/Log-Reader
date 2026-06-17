574092e5e43f490945c73392201b76809bfab272
Naviga <shivangisrivastava137@gmail.com>
2026-06-17 15:07:30 +0530

    updated

--- trishu.py
+++ trishu.py
@@ -4,6 +4,7 @@ import os
 from datetime import datetime
 import pandas as pd
 
+# Testing
 
 #========== STYLING THE PAGE ==========
 st.set_page_config(
@@ -262,7 +263,3 @@ if "result_df" in st.session_state:
     )
 
     st.success(f"{len(df)} records found")
-
-
-            
-
